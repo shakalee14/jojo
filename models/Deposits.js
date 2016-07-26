c2df01d4ebc5a1bd9869f8e991f52f8c86820087
John Hopkins <johnthopkins@gmail.com>
2016-07-26 11:14:39 -0700

    nailed down models

---
+++ models/Deposits.js
@@ -0,0 +1,26 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * Deposits Model
+ * ==========
+ */
+var Deposits = new keystone.List('Deposits');
+
+Deposits.add({
+	members: { type: Types.Relationship, ref: 'Member', initial: true},
+	amount: { type: Types.Money, required: true, initial: true},
+	startDate: { type: Types.Date, initial: Date.now(), required: true}
+});
+
+// Provide access to Keystone
+Deposits.schema.virtual('canAccessKeystone').get(function () {
+	return this.isAdmin;
+});
+
+
+/**
+ * Registration
+ */
+Deposits.defaultColumns = 'name, account, dsr, startDate';
+Deposits.register();
--- models/Susu.js
+++ models/Susu.js
@@ -2,50 +2,19 @@ var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 /**
- * Member Model
+ * Susu Model
  * ==========
  */
 var Susu = new keystone.List('Susu');
 
 Susu.add({
-	member: { type: Types.Relationship, ref: 'Member' },
-	name: { type: Types.Name, required: true, index: true },
-	account : { type: Types.Number, initial: null, required: true, index: true },
-	startDate: { type: Types.Date, initial: Date.now(), required: true },
-	dsr: {type: Types.Money, required: true, initial: 0.0},
-	day01: {type: Types.Boolean, required: false},
-	day02: {type: Types.Boolean, required: false},
-	day03: {type: Types.Boolean, required: false},
-	day04: {type: Types.Boolean, required: false},
-	day05: {type: Types.Boolean, required: false},
-	day06: {type: Types.Boolean, required: false},
-	day07: {type: Types.Boolean, required: false},
-	day08: {type: Types.Boolean, required: false},
-	day09: {type: Types.Boolean, required: false},
-	day10: {type: Types.Boolean, required: false},
-	day11: {type: Types.Boolean, required: false},
-	day12: {type: Types.Boolean, required: false},
-	day13: {type: Types.Boolean, required: false},
-	day14: {type: Types.Boolean, required: false},
-	day15: {type: Types.Boolean, required: false},
-	day16: {type: Types.Boolean, required: false},
-	day17: {type: Types.Boolean, required: false},
-	day18: {type: Types.Boolean, required: false},
-	day19: {type: Types.Boolean, required: false},
-	day20: {type: Types.Boolean, required: false},
-	day21: {type: Types.Boolean, required: false},
-	day22: {type: Types.Boolean, required: false},
-	day23: {type: Types.Boolean, required: false},
-	day24: {type: Types.Boolean, required: false},
-	day25: {type: Types.Boolean, required: false},
-	day26: {type: Types.Boolean, required: false},
-	day27: {type: Types.Boolean, required: false},
-	day28: {type: Types.Boolean, required: false},
-	day29: {type: Types.Boolean, required: false},
-	day30: {type: Types.Boolean, required: false},
-	day31: {type: Types.Boolean, required: false},
-}, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true },
+	title: { type: Types.Text, required: true, index: true , initial: true},
+	description: { type: Types.Textarea, required: true, index: true, initial: true},
+	members: { type: Types.Relationship, ref: 'Member', initial: true, many: true},
+	share: { type: Types.Money, required: true, initial: true},
+	deposits: {type: Types.Relationship, ref: 'Deposits'},
+	withdrawals: {type: Types.Relationship, ref: 'Withdrawals'},
+	startDate: { type: Types.Date, initial: Date.now(), required: true }
 });
 
 // Provide access to Keystone
